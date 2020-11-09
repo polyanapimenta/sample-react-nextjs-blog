b42e5ec5db2d21a25dcaa7a28c802081c2e80786
Polyana L. Pimenta <polyana.pimenta@gmail.com>
2020-11-09 17:19:32 -0300

    Updating index page

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,6 @@ export default function Home({allPostsData}) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Aplicação de teste para aprender as funcionalidades básicas do Next.js</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
